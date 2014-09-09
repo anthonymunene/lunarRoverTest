30de67de0855e9d1cc8e06c0a522941dc915e752
Anthony Munene <munene.anthony@gmail.com>
2014-09-09 10:27:58 +0100

    improved comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Create the Moon, Lander and Rover as separate objects using whatever approach you feel comfortable using.
 //Give the objects the methods to perform the tasks outlined below.
 
+//'Land' as a concept simply means the element is appended to the other element.
+
 
 // The Lander can land on the Moon
 // The Lander will land in a random position on the Moon
